7e5f7c01bfd3d2751ec73c0ed491517c2f87db8d
Hazel Lindsey <clindsey7266@floridapoly.edu>
2025-11-12 20:32:17 +0000

    added player input

--- game.py
+++ game.py
@@ -1,1 +1,11 @@
 print("Welcome to rock, paper, scissors")
+player = input("Choose rock, paper, or scissors: ")
+player = player.lower()
+
+while True:
+    if (player != "rock" and player != "paper" and player != "scissors"):
+        player = input("Choose rock, paper, or scissors: ")
+        player = player.lower()
+    else:
+        print(f"You chose: {player}.")
+        break
